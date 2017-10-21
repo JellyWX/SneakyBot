521e19840cfecf70797e34d3d5125f36e98919d3
MrYetiGuy <32039460+MrYetiGuy@users.noreply.github.com>
2017-10-21 18:33:39 +0100

    Basic setup
    Please work off this basic setup it is labelled easily for you to use. Make sure you send pull requests and not edit it directly.

---
+++ bot.py
@@ -0,0 +1,34 @@
+###############Imports################
+import discord
+from discord.ext import commands
+###############Defining the variables################
+initial_extensions = ('')
+
+
+                      
+bot = commands.Bot(command_prefix=';', description='A bot for sneaky's help server!')
+
+##########On ready#######################
+
+@bot.event
+async def on_ready():
+    print('\n\nLogged in as: {} - {}\nVersion: {}\n'.format(bot.user.name, bot.user.id, discord.__version__))
+    await bot.change_presence(game=discord.Game(name=' ;help | Moderating the server!'.))
+    
+    
+    
+#########Cog loader###########################
+
+
+        if __name__ == '__main__':
+        for extension in initial_extensions:
+            try:
+                bot.load_extension(extension)
+            except Exception as e:
+                print('Failed to load extension {}.'.format(extension), file=sys.stderr)
+                traceback.print_exc()
+    print('Successfully logged in and booted...!')
+    
+###########Token##############################
+
+bot.run('token', bot=True, reconnect=True)
