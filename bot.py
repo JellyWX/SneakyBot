2926e5fd3889eea74853bf67877c01c406f93e41
Altarrel <31830955+Altarrel@users.noreply.github.com>
2017-12-04 16:08:24 -0500

    NOT FINISHED: Added submit command

--- bot.py
+++ bot.py
@@ -7,24 +7,25 @@ import traceback
 with open("token.txt", 'r') as f:
     token = f.read().strip('\n')
 
-    
-#Stuff
-initial_extensions = ('cogs.Admin', 'cogs.fun')
+
+#Cogs/extensions to load when the script is run (is that proper grammar?)
+initial_extensions = ('cogs.Admin', 'cogs.botsubmit')
 
 
-                      
+
 bot = commands.Bot(command_prefix='!', description='A bot for sneakys help server!')
 bot.remove_command("help")
 
 #The first step to a great bot! On ready events!
+#Nah
 
 @bot.event
 async def on_ready():
-    print('\n\nLogged in as: {} - {}\nVersion: {}\n'.format(bot.user.name, bot.user.id, discord.__version__))
-    await bot.change_presence(game=3(name='The server..'))
-    
-    
-    
+    print('\n\nLogged in as: {} - {}\nDiscord Version: {}\n'.format(bot.user.name, bot.user.id, discord.__version__))
+    await bot.change_presence(game=discord.Game(name='Moderating the server!'))
+
+
+
 #Cog Loader
 
 
@@ -36,7 +37,7 @@ if __name__ == '__main__':
             print('Failed to load extension {}.'.format(extension))
             traceback.print_exc()
         print('Successfully logged in and booted...!')
-    
-#The super super super token that is locked away by ninjas and flying penguins?
+
+#The super super super token that is locked away by ninjas and flying penguins? Quack quack Altarrel is a duck.
 
 bot.run(token)
